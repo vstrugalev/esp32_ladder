3ff14ecb0a713ec477209c254aec0ab3d4472702
Vladimir <vstrugalev@gmail.com>
2024-02-17 19:09:28 +0300

    Create ladder.h

---
+++ ladder.h
@@ -0,0 +1,22 @@
+class MyCustomLightOutput : public Component, public LightOutput {
+ public:
+  void setup() override {
+    // This will be called by App.setup()
+    pinMode(5, INPUT);
+  }
+  LightTraits get_traits() override {
+    // return the traits this light supports
+    auto traits = LightTraits();
+    traits.set_supported_color_modes({ColorMode::RGB, ColorMode::BRIGHTNESS});
+    return traits;
+  }
+
+  void write_state(LightState *state) override {
+    // This will be called by the light to get a new state to be written.
+    float red, green, blue;
+    // use any of the provided current_values methods
+    state->current_values_as_rgb(&red, &green, &blue);
+    // Write red, green and blue to HW
+    // ...
+  }
+};
